32a5fdb5602d233d3fe43c4ec3e94f3d3ec807a4
Saksham Kulshrestha <sakshamkul224@gmail.com>
2025-01-01 23:17:04 +0530

    added tailwind and shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
     <>
-      <h1>Dtox- Try not being too Toxic </h1>
+      <h1 className="text-3xl bg-slate-400 font-bold underline">Dtox- Try not being too Toxic </h1>
+      <Button>Click me</Button>
     </>
   )
 }
