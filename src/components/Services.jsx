b1a8ef59864c00a976c9aad56a030388aaff7bd5
Anurag singh bhagour <148285491+anuragbhagour@users.noreply.github.com>
2025-01-12 16:20:10 +0530

    changed service.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div className='text-2xl text-red-600'>Service page</div>
+    <div className='text-2xl text-red-600'>Service page OF DTOX</div>
   )
 }
 
